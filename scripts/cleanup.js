edf78bfdf7de264bb8e91871915fceece0505446
tkansaki <90021981+tkansaki@users.noreply.github.com>
2025-10-14 14:41:43 -1000

    UI for cleanup tool

--- scripts/cleanup.js
+++ scripts/cleanup.js
@@ -1,8 +1,27 @@
-import {XLSX} from './xlsx.js';
+import { XLSX } from './xlsx.js';
 
 document.getElementById("uploadInput").addEventListener("change", (event) => {
   handleDropAsync(event);
 });
+
+function printTable(sheet) {
+	let rowHead = document.createElement("tr");
+	for (let j = 0; j < 3; j++) {
+		let data = document.createElement("th")
+		data.innerHTML = sheet[`${String.fromCharCode(65 + j)}1`].v
+		rowHead.appendChild(data)
+	}
+	document.getElementById("mainTable").appendChild(rowHead);
+	for (let i = 2; i <= 10; i++) {
+		let row = document.createElement("tr");
+		for (let j = 0; j < 3; j++) {
+			let data = document.createElement("td")
+			data.innerHTML = sheet[`${String.fromCharCode(65 + j)}${i}`].v
+			row.appendChild(data)
+		}
+		document.getElementById("mainTable").appendChild(row);
+	}
+}
 
 async function handleDropAsync(e) {
   console.log("called");
@@ -13,5 +32,5 @@ async function handleDropAsync(e) {
   /* data is an ArrayBuffer */
   const workbook = XLSX.read(data);
 
-  console.log(workbook.Sheets["Sheet1"]);
+  printTable(workbook.Sheets["Sheet1"]);
 }
