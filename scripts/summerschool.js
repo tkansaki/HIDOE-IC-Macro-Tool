dd7fc6e33e4f6fbf9fe0b419346fbd411ac7628a
tkansaki <90021981+tkansaki@users.noreply.github.com>
2026-04-16 15:18:48 -1000

    Update summerschool.js

--- scripts/summerschool.js
+++ scripts/summerschool.js
@@ -1,5 +1,7 @@
 import schools from "./schools.js"
 
+let data;
+
 document.addEventListener('DOMContentLoaded', function() {
     let options = [];
     options.push(`<option selected="selected">Choose School</option>`);
@@ -12,15 +14,42 @@ document.addEventListener('DOMContentLoaded', function() {
     });
 });
 
+function printTable() {
+    let range = data.length;
+    let rowHead = document.createElement("tr");
+    for (let j = 1; j <= 3; j++) {
+        let data = document.createElement("th");
+        if (sheet[`${intToletterBase26(j)}1`]) {
+            data.innerHTML = sheet[`${intToletterBase26(j)}1`].v
+        }
+        rowHead.appendChild(data)
+    }
+    document.getElementById("mainTable").appendChild(rowHead);
+    for (let i = 2; i <= range.rowMax; i++) {
+        let row = document.createElement("tr");
+        for (let j = 1; j <= 3; j++) {
+            let data = document.createElement("td")
+            if (sheet[`${intToletterBase26(j)}${i}`]) {
+                data.innerHTML = sheet[`${intToletterBase26(j)}${i}`].v
+            }
+            row.appendChild(data)
+        }
+        document.getElementById("mainTable").appendChild(row);
+    }
+}
+
 function parse() {
     const text = document.getElementById('pastedList').value;
     const school = schools[document.getElementById('school').value];
     const rows = text.split('\n');
-    const data = [];
+    data = [];
     for (let i = 0; i < rows.length; i++) {
         data.push(rows[i].split('\t'));
     }
     // console.log(data);
+}
+
+function start() {
     chrome.tabs.query({
         currentWindow: true,
         active: true
